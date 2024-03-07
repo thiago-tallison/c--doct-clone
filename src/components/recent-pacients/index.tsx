0465e7c5d302b594d0ddc0fbc48cdc37885c3997
Thiago Tallison <thiagotallison@gmail.com>
2024-03-07 14:37:56 -0300

    refactor: add Recent Patients title to its section

--- src/components/recent-pacients/index.tsx
+++ src/components/recent-pacients/index.tsx
@@ -1,6 +1,6 @@
 import { toHumanReadableDate } from '@/utils/format-date'
 import { createRandomPatient } from '@/utils/get-random-user'
-import { MoreVertical } from 'lucide-react'
+import { ChevronRight, MoreVertical } from 'lucide-react'
 import Image from 'next/image'
 
 export type RecentPatientType = {
@@ -15,54 +15,61 @@ export type RecentPatientType = {
 
 export function RecentPatients() {
   return (
-    <table className='mt-6 border-separate border-spacing-y-2'>
-      <thead>
-        <tr className='bg-gray-200 text-left'>
-          <th className='rounded-tl-md rounded-bl-md py-4 pl-5'>
-            Patient name
-          </th>
-          <th className='py-4 pl-5'>Vist Id</th>
-          <th className='py-4 pl-5'>Date</th>
-          <th className='py-4 pl-5'>Gender</th>
-          <th className='py-4 pl-5'>Diseases</th>
-          <th className='py-4 pl-5'>Status</th>
-          <th className='rounded-tr-md rounded-br-md py-4'></th>
-        </tr>
-      </thead>
+    <div className='flex flex-col w-full mt-6'>
+      <div className="flex justify-between items-center mb-4">
+        <strong>Recent Patients</strong>
+        <a href="#" className="flex items-center text-blue-500 font-semibold text-sm hover:underline">View All <ChevronRight size={20} /></a>
+      </div>
 
-      <tbody className='text-sm text-text-secondary'>
-        {Array.from({ length: 5 }).map(() => {
-          const patient = createRandomPatient()
-          const { ISODate, diseases, gender, imageURL, status, userName, vistId } = patient
+      <table className='border-separate border-spacing-y-2'>
+        <thead>
+          <tr className='bg-gray-200 text-left'>
+            <th className='rounded-tl-md rounded-bl-md py-4 pl-5'>
+              Patient name
+            </th>
+            <th className='py-4 pl-5'>Vist Id</th>
+            <th className='py-4 pl-5'>Date</th>
+            <th className='py-4 pl-5'>Gender</th>
+            <th className='py-4 pl-5'>Diseases</th>
+            <th className='py-4 pl-5'>Status</th>
+            <th className='rounded-tr-md rounded-br-md py-4'></th>
+          </tr>
+        </thead>
 
-          return (
-            <tr
-              key={vistId}
-              className='bg-white shadow-md hover:bg-muted'>
-              <td className='!rounded-tl-md !rounded-bl-md py-4 pl-5 flex items-center gap-2'>
-                <Image
-                  className='w-7 h-7 object-cover rounded-full'
-                  src={imageURL}
-                  width={280}
-                  height={280}
-                  alt={`${userName}'s avatar`}
-                />
-                <span>{userName}</span>
-              </td>
-              <td className='py-4 pl-5'>{vistId}</td>
-              <td className='py-4 pl-5'>{toHumanReadableDate(ISODate)}</td>
-              <td className='py-4 pl-5'>{gender}</td>
-              <td className='py-4 pl-5'>{diseases}</td>
-              <td className='py-4 pl-5'>{status}</td>
-              <td className='rounded-tr-md rounded-br-md py-4'>
-                <button type='button'>
-                  <MoreVertical size={20} />
-                </button>
-              </td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+        <tbody className='text-sm text-text-secondary'>
+          {Array.from({ length: 5 }).map(() => {
+            const patient = createRandomPatient()
+            const { ISODate, diseases, gender, imageURL, status, userName, vistId } = patient
+
+            return (
+              <tr
+                key={vistId}
+                className='bg-white shadow-md hover:bg-muted'>
+                <td className='!rounded-tl-md !rounded-bl-md py-4 pl-5 flex items-center gap-2'>
+                  <Image
+                    className='w-7 h-7 object-cover rounded-full'
+                    src={imageURL}
+                    width={280}
+                    height={280}
+                    alt={`${userName}'s avatar`}
+                  />
+                  <span>{userName}</span>
+                </td>
+                <td className='py-4 pl-5'>{vistId}</td>
+                <td className='py-4 pl-5'>{toHumanReadableDate(ISODate)}</td>
+                <td className='py-4 pl-5'>{gender}</td>
+                <td className='py-4 pl-5'>{diseases}</td>
+                <td className='py-4 pl-5'>{status}</td>
+                <td className='rounded-tr-md rounded-br-md py-4'>
+                  <button type='button'>
+                    <MoreVertical size={20} />
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 }
