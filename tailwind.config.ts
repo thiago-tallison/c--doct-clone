2325e620e72fd94b0b068a632295d8dd7277ff59
Thiago Tallison <thiagotallison@gmail.com>
2024-03-07 10:08:42 -0300

    feat: configure colors and define page structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        dark: "#0C0B19",
+        muted: "#F9F9F9",
+        text: {
+          primary: "#1B2531",
+          secondary: "#62676B",
+        },
+        purple: {
+          500: "#7A6EFE",
+          300: "#9188FF"
+        },
+        yellow: {
+          500: "#FFA901",
+          300: "#FFB630"
+        },
+        blue: {
+          500: "#24A8FA",
+          300: "#4BB7FD"
+        },
+        red: {
+          500: "#FF5363",
+          300: "#FF717F",
+        }
+      }
     },
   },
   plugins: [],
